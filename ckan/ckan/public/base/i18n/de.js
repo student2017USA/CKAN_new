a75193a311d2586d26cbc6f098825b03add7dbbc
shribrainium <124235350+shribrainium@users.noreply.github.com>
2023-02-20 15:45:38 +0530

    updated missing code

---
+++ ckan/ckan/public/base/i18n/de.js
@@ -0,0 +1,196 @@
+{
+  "": {
+    "domain": "ckan",
+    "lang": "de",
+    "plural-forms": "nplurals=2; plural=(n != 1);"
+  },
+  "Add Filter": [
+    null,
+    "Filter hinzufügen"
+  ],
+  "An Error Occurred": [
+    null,
+    "Es trat ein Fehler auf"
+  ],
+  "An error occured during AJAX request. Could not load view.": [
+    null,
+    "Ein Fehler ist aufgetreten bei einer AJAX Anforderung. Die Ansicht konnte nicht geladen werden."
+  ],
+  "Are you sure you want to perform this action?": [
+    null,
+    "Sind Sie sicher, dass Sie diese Aktion ausführen wollen?"
+  ],
+  "Cancel": [
+    null,
+    "Abbrechen"
+  ],
+  "Confirm": [
+    null,
+    "Bestätigen"
+  ],
+  "Could not load view": [
+    null,
+    "Ansicht konnte nicht geladen werden"
+  ],
+  "DataProxy returned an error": [
+    null,
+    "DataProxy gab einen Fehler zurück"
+  ],
+  "DataStore returned an error": [
+    null,
+    "DataStore gab einen Fehler zurück"
+  ],
+  "Edit": [
+    null,
+    "Bearbeiten"
+  ],
+  "Failed to load data API information": [
+    null,
+    "Informationen zur Daten-API konnten nicht geladen werden"
+  ],
+  "File": [
+    null,
+    "Datei"
+  ],
+  "Filters": [
+    null,
+    "Filter"
+  ],
+  "Follow": [
+    null,
+    "Folgen"
+  ],
+  "Graph": [
+    null,
+    "Graph"
+  ],
+  "Grid": [
+    null,
+    "Raster"
+  ],
+  "Hide": [
+    null,
+    "Verstecken"
+  ],
+  "Image": [
+    null,
+    "Bild"
+  ],
+  "Input is too short, must be at least one character": [
+    "Input is too short, must be at least %(num)d characters",
+    "Die Eingabe muss mindestens ein Zeichen lang sein",
+    "Die Eingabe muss mindestens %(num)d Zeichen lang sein"
+  ],
+  "Link": [
+    null,
+    "Link"
+  ],
+  "Link to a URL on the internet (you can also link to an API)": [
+    null,
+    "Link zu einer URL im Internet (Sie können auch den Link zu einer API angeben)"
+  ],
+  "Loading...": [
+    null,
+    "Lädt..."
+  ],
+  "Map": [
+    null,
+    "Karte"
+  ],
+  "No matches found": [
+    null,
+    "Keine Treffer"
+  ],
+  "Please Confirm Action": [
+    null,
+    "Bestätige bitte die Aktion"
+  ],
+  "Please select the file to upload again": [
+    null,
+    "Bitte Datei auswählen und erneut hochladen"
+  ],
+  "Remove": [
+    null,
+    "Entfernen"
+  ],
+  "Reorder resources": [
+    null,
+    "Ressourcen neu ordnen"
+  ],
+  "Resource uploaded": [
+    null,
+    "Ressource hochgeladen"
+  ],
+  "Save order": [
+    null,
+    "Reihenfolge speichern"
+  ],
+  "Saving...": [
+    null,
+    "Speichern..."
+  ],
+  "Select a field": [
+    null,
+    "Wählen Sie ein Feld"
+  ],
+  "Show more": [
+    null,
+    "Mehr anzeigen"
+  ],
+  "Start typing…": [
+    null,
+    "Gib etwas ein..."
+  ],
+  "There are unsaved modifications to this form": [
+    null,
+    "Dieses Formular hat ungespeicherte Änderungen"
+  ],
+  "There is no API data to load for this resource": [
+    null,
+    "Diese Ressource hat keine API-Daten zum Laden"
+  ],
+  "URL": [
+    null,
+    "URL"
+  ],
+  "Unable to authenticate upload": [
+    null,
+    "Upload konnte nicht autentifiziert werden"
+  ],
+  "Unable to get data for uploaded file": [
+    null,
+    "Daten für hochgeladen Datei konnten nicht abgerufen werden"
+  ],
+  "Unable to upload file": [
+    null,
+    "Datei konnte nicht hochgeladen werden"
+  ],
+  "Unfollow": [
+    null,
+    "Entfolgen"
+  ],
+  "Upload": [
+    null,
+    "Hochladen"
+  ],
+  "Upload a file": [
+    null,
+    "Datei hochladen"
+  ],
+  "Upload a file on your computer": [
+    null,
+    "Datei von Ihrem Computer hochladen"
+  ],
+  "You are uploading a file. Are you sure you want to navigate away and stop this upload?": [
+    null,
+    "Eine Datei wird hochgeladen. Soll das wirklich abgebrochen werden?"
+  ],
+  "You can rearrange the resources by dragging them using the arrow icon. Drag the resource to the right and place it to the desired location on the list. When you are done, click the \"Save order\" -button.": [
+    null,
+    "Sie können die Ressourcen neu anordnen, in dem sie mit dem Pfeil-Icon verschoben werden. Ziehen sie die Ressource nach rechts auf die gewünschte Stelle in der Liste. Wenn Sie fertig sind, klicken sie den Speichern-Button"
+  ],
+  "error loading view": [
+    null,
+    "Fehler beim Laden der Ansicht"
+  ]
+}
---
+++ ckan/ckan/public/base/i18n/he.js
@@ -0,0 +1,198 @@
+{
+  "": {
+    "domain": "ckan",
+    "lang": "he",
+    "plural-forms": "nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;"
+  },
+  "Add Filter": [
+    null,
+    "הוספת מסנן"
+  ],
+  "An Error Occurred": [
+    null,
+    "אירעה שגיאה"
+  ],
+  "An error occured during AJAX request. Could not load view.": [
+    null,
+    "אירעה שגיאה עם בקשת ה־AJAX. לא ניתן לטעון את התצוגה."
+  ],
+  "Are you sure you want to perform this action?": [
+    null,
+    "לבצע את הפעולה הזו?"
+  ],
+  "Cancel": [
+    null,
+    "ביטול"
+  ],
+  "Confirm": [
+    null,
+    "אישור"
+  ],
+  "Could not load view": [
+    null,
+    "לא ניתן לטעון את התצוגה"
+  ],
+  "DataProxy returned an error": [
+    null,
+    "מתווך הנתונים (DataProxy) החזיר שגיאה"
+  ],
+  "DataStore returned an error": [
+    null,
+    "חנות הנתונים (DataStore) החזירה שגיאה"
+  ],
+  "Edit": [
+    null,
+    "לערוך"
+  ],
+  "Failed to load data API information": [
+    null,
+    "טעינת פרטי ה־API של הנתונים נכשלה"
+  ],
+  "File": [
+    null,
+    "קובץ"
+  ],
+  "Filters": [
+    null,
+    "מסננים"
+  ],
+  "Follow": [
+    null,
+    "לעקוב"
+  ],
+  "Graph": [
+    null,
+    "תרשים"
+  ],
+  "Grid": [
+    null,
+    "רשת"
+  ],
+  "Hide": [
+    null,
+    "הסתרה"
+  ],
+  "Image": [
+    null,
+    "תמונה"
+  ],
+  "Input is too short, must be at least one character": [
+    "Input is too short, must be at least %(num)d characters",
+    "הקלט קצר מדי, חייב להיות באורך של תו אחד לפחות",
+    "הקלט קצר מדי, חייב להיות באורך של %(num)d תווים לפחות",
+    "הקלט קצר מדי, חייב להיות באורך של %(num)d תווים לפחות",
+    "הקלט קצר מדי, חייב להיות באורך של %(num)d תווים לפחות"
+  ],
+  "Link": [
+    null,
+    "קישור"
+  ],
+  "Link to a URL on the internet (you can also link to an API)": [
+    null,
+    "קישור לכתובת באינטרנט (ניתן לקשר גם ל־API)"
+  ],
+  "Loading...": [
+    null,
+    "בטעינה..."
+  ],
+  "Map": [
+    null,
+    "מפה"
+  ],
+  "No matches found": [
+    null,
+    "לא נמצאו התאמות"
+  ],
+  "Please Confirm Action": [
+    null,
+    "נא לאשר את הפעולה"
+  ],
+  "Please select the file to upload again": [
+    null,
+    "נא לבחור את הקובץ כדי להעלות שוב"
+  ],
+  "Remove": [
+    null,
+    "הסרה"
+  ],
+  "Reorder resources": [
+    null,
+    "לסדר משאבים מחדש"
+  ],
+  "Resource uploaded": [
+    null,
+    "המשאב הועלה"
+  ],
+  "Save order": [
+    null,
+    "שמירת הסדר"
+  ],
+  "Saving...": [
+    null,
+    "מתבצעת שמירה…"
+  ],
+  "Select a field": [
+    null,
+    "בחירת שדה"
+  ],
+  "Show more": [
+    null,
+    "להציג עוד"
+  ],
+  "Start typing…": [
+    null,
+    "ניתן להקליד כאן…"
+  ],
+  "There are unsaved modifications to this form": [
+    null,
+    "ישנם שינויים שלא נשמרו לטופס הזה"
+  ],
+  "There is no API data to load for this resource": [
+    null,
+    "אין נתוני API כדי לטעון את המשאב הזה"
+  ],
+  "URL": [
+    null,
+    "כתובת"
+  ],
+  "Unable to authenticate upload": [
+    null,
+    "לא ניתן לאמת את ההעלאה"
+  ],
+  "Unable to get data for uploaded file": [
+    null,
+    "לא ניתן לקבל נתונים מהקובץ שהועלה"
+  ],
+  "Unable to upload file": [
+    null,
+    "לא ניתן להעלות קובץ"
+  ],
+  "Unfollow": [
+    null,
+    "ביטול מעקב"
+  ],
+  "Upload": [
+    null,
+    "העלאה"
+  ],
+  "Upload a file": [
+    null,
+    "העלאת קובץ"
+  ],
+  "Upload a file on your computer": [
+    null,
+    "העלאת קובץ מהמחשב שלך"
+  ],
+  "You are uploading a file. Are you sure you want to navigate away and stop this upload?": [
+    null,
+    "כרגע נשלח ממך קובץ למערכת. לנווט אל מחוץ לעמוד ולעצור את ההעלאה?"
+  ],
+  "You can rearrange the resources by dragging them using the arrow icon. Drag the resource to the right and place it to the desired location on the list. When you are done, click the \"Save order\" -button.": [
+    null,
+    "ניתן לשנות את סדר המשאבים על ידי גרירתם באמצעות סמל החץ. ניתן לגרור את המשאבים לשמאל ולהציב אותם במיקום המבוקש ברשימה. כשסיימת, יש ללחוץ על כפתור „שמירת הסדר”."
+  ],
+  "error loading view": [
+    null,
+    "טעינת התצוגה נכשלה"
+  ]
+}
